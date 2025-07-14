495c953905193969082abb901fe5253a34d798cc
Ashom Azi <ashomazibulus@gmail.com>
2025-07-14 23:45:43 +0100

    Added the Logout Logic

--- Register/urls.py
+++ Register/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.register, name='register'),
     path('home', views.home, name='home'),
     path('login', views.login, name='login'),
+    path('logout', views.logout, name='logout'),
 ]
